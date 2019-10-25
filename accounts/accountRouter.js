d4ce593f67c44a6c5eb961cc18637039d5daa928
BuildTools <arthurpisakhov1@gmail.com>
2019-10-24 22:32:53 -0400

    MVP done

--- accounts/accountRouter.js
+++ accounts/accountRouter.js
@@ -8,15 +8,40 @@ router.get('/', (req, res) => {
     .catch(err => res.status(500).json(err))
 })
 
-router.post('/', accountIsValid, (req, res) => {
-  const { name, budget } = req.body;
-
-  db('accounts').insert({ name: name, budget: budget})
-    .then(account => res.status(201).json({ message: 'Added new budget' }))
-    .catch(err => res.status(500).json({ message: 'Failed to add account' }))
+router.get('/:id', (req, res) => {
+  db('accounts').where({ id: req.params.id })
+    .then(account => {
+      if (account.length > 0) res.status(200).json(account)
+      else res.status(404).json({ message: 'account does not exist' })
+    })
+    .catch(err => res.status(500).json(err))
 })
 
-function accountIsValid(req, res, next) {
+router.post('/', fieldsAreValid, (req, res) => {
+  const { name, budget } = req.body;
+
+  db('accounts').insert({ name: name, budget: budget })
+    .then(account => res.status(201).json(account))
+    .catch(err => res.status(500).json(err))
+})
+
+router.put('/:id', fieldsAreValid, (req, res) => {
+  db('accounts')
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(account => res.status(201).json(account))
+    .catch(err => res.status(500).json({ error: 'Failed to update.' }))
+})
+
+router.delete('/:id', (req, res) => {
+  db('accounts')
+    .where({ id: req.params.id })
+    .del()
+    .then(() => res.status(202).json({ error: `The account with the ID ${req.params.id} has been removed.` }))
+    .catch(() => res.status(500).json({ error: 'Failed to update.' }))
+})
+
+function fieldsAreValid(req, res, next) {
   const { name, budget } = req.body;
 
   if (!req.body) res.status(400).json({ message: "missing fields data" })
